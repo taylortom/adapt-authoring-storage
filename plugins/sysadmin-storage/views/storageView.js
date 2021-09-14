a2351e1adf73f71c02f257d302f2b77e84f83594
Tom Taylor <hello@tomtaylor.name>
2021-09-14 17:07:26 +0100

    Add front-end files

---
+++ plugins/sysadmin-storage/views/storageView.js
@@ -0,0 +1,42 @@
+define([
+	"core/origin",
+	"plugins/sysadmin/views/sysadminPluginView"
+], function(Origin, SysadminPluginView) {
+
+	var StorageView = SysadminPluginView.extend({
+
+		name: "storage",
+
+		events: {
+			"click .plugins button": "onPluginsClick"
+		},
+
+		preRender: function() {
+			$.ajax({
+				url: "api/storage",
+				error: function(jqXHR, textStatus, errorThrown) {
+					Origin.Notify.alert({
+						type: "error",
+						text: Origin.l10n.t("app.storageerror")
+					});
+				},
+				success: function(data, textStatus, jqXHR) {
+					this.model = new Backbone.Model(data);
+					this.render();
+				}.bind(this)
+			});
+		},
+
+		postRender: function() {
+			this.setViewToReady();
+		},
+
+		onPluginsClick: function() {
+			Origin.router.navigateTo("pluginManagement");
+		}
+
+	}, { template: "storage" });
+
+	return StorageView;
+
+});
